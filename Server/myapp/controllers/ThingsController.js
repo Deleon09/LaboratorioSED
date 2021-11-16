39b357734bceabd97667be4e9be83533c717ac2d
Alberto <alberto101481@gmail.com>
2021-11-16 16:54:25 -0600

    ADD: Api finished

--- Server/myapp/controllers/ThingsController.js
+++ Server/myapp/controllers/ThingsController.js
@@ -19,6 +19,68 @@ var ThingsController = {
             return res.status(400).json(err);
         }
     },
+
+    deleteT: async (req, res) => {
+        try {
+            var supr = await Things.findOneAndDelete({
+              _id: req.body._id
+            });
+  
+            return res.status(200).json({ obj: supr });
+            
+        } catch (err) {
+              return res.status(400).json(err);
+        }
+    },
+
+    update: async(req, res) => {
+        try{
+           var upt = await Things.updateOne({name: req.body.name},
+              {name: req.body.newName});
+  
+              return res.status(200).json({ obj: upt });
+  
+        } catch(err){
+              return res.status(400).json(err);
+        }
+    },
+
+    getAll: async (req, res) => {
+        try{
+            const { page = 1, limit = 10 } = req.query;
+    
+            const things = await Things.find()
+                .limit(limit * 1)
+                .skip((page - 1) * limit)
+                .exec()
+            
+            const count = await Things.countDocuments();
+    
+            return res.status(200).json({
+                pages: Math.ceil(count / limit),
+                current: page,
+                things
+            });
+        }
+        catch(err){
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    },
+
+    getAllThings: async (req, res) => {
+        try{
+            const things = await Things.find();
+            
+            return res.status(200).json({
+                things
+            });
+        }
+        catch(err){
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
 };
 
 module.exports = ThingsController;
--- Server/myapp/routes/ThingsRouter.js
+++ Server/myapp/routes/ThingsRouter.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-const { create } = require('../controllers/ThingsController');
+const { create, deleteT, update, getAll, getAllThings } = require('../controllers/ThingsController');
 
+router.get("/", getAll);
+router.get("/all", getAllThings);
 router.post('/create', create);
+router.delete('/delete', deleteT);
+router.post('/updateproduct', update);
 
 module.exports = router;
